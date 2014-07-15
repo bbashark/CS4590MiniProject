d914b7a9e973eedd6ea5a42167cc1a61b236e424
mbradley36 <mirandajbradley@gmail.com>
2014-07-15 10:11:24 -0400

    re-organized, added lost things, script skeletons
    Alert and Alert manager scripts, lights re-added, prefabs moved into
    folder

---
+++ MiniProject/Assets/Scripts/Alert.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alert: MonoBehaviour {
+
+	public bool randomAlert;
+
+	public void ActivateAlert(){
+
+	}
+
+	public void ResolveAlert(){
+
+	}
+
+}
---
+++ MiniProject/Assets/Scripts/AlertManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AlertManager : MonoBehaviour {
+
+	public Alert[] alerts;
+	private Alert[] randomAlerts;
+	private bool pickRandomAlert;
+
+
+	// Use this for initialization
+	void Start () {
+		//cycle through alerts, if alert isn't intended to be triggered at a particular time,
+		//add it to random alerts
+		//.RandomAlert();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CallAlert(Alert alertToCall){
+		AddAlertVisual ();
+		alertToCall.ActivateAlert ();
+	}
+
+	public void ClearAlert(Alert alertToClear){
+		alertToCall.ResolveAlert ();
+		RemoveAlertVisual ();
+	}
+
+	public void RemoveAlertVisual(){
+
+	}
+
+	public void AddAlertVisual(){
+
+	}
+	
+}
