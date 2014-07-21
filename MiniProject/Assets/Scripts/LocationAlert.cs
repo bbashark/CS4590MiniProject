26006f32b95f1e5a559ad7eec6f8a192729490d5
mbradley36 <mirandajbradley@gmail.com>
2014-07-21 11:11:15 -0400

    location alert progress

--- MiniProject/Assets/Scripts/LocationAlert.cs
+++ MiniProject/Assets/Scripts/LocationAlert.cs
@@ -8,8 +8,10 @@ public class LocationAlert : Alert {
 	private GameObject positionCollider;
 	private GameObject player;
 	private bool active = false;
+	private float pauseGap, startPause, startPauseTime, pause;
 
 	public override void ActivateAlert() {
+		startPauseTime = Time.time;
 		active = true;
 		Debug.Log ("also do new activate stuff");
 		base.ActivateAlert ();
@@ -34,18 +36,28 @@ public class LocationAlert : Alert {
 
 	// Use this for initialization
 	void Start () {
+		startPause = activateClip.length;
+		pauseGap = 0.6f;
 		player = GameObject.FindGameObjectWithTag("Player");
 		randomAlert = true;
 	}
 
 	void Update() {
-		if(active) {
+		if(active && Time.time - startPauseTime > startPause) {
 			//run location finding clip with pitch changing based on distance
 			float xDist = Mathf.Abs(positionCollider.transform.position.x - player.transform.position.x);
 			xDist *= xDist;
 			float zDist = Mathf.Abs(positionCollider.transform.position.z - player.transform.position.z);
 			zDist *= zDist;
 			float dist = Mathf.Sqrt(xDist + zDist);
+
+			if(!audio.isPlaying && pause > pauseGap) {
+				pause = 0;
+				audio.clip = locationFindingClip;
+				audio.Play();
+			} else if(!audio.isPlaying){
+				pause += 0.1f;
+			}
 
 			Debug.Log ("distance: "+dist);
 		}
